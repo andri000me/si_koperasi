5c522a841e1e2679d1f72a5b0012e0d5e76c2936
Elsa manora <elsaramadania@gmail.com>
2019-11-06 00:29:22 +0700

    edit form kredit

--- application/controllers/Kredit.php
+++ application/controllers/Kredit.php
@@ -1,27 +1,47 @@
 <?php
 defined("BASEPATH") or die("No Direct Access Allowed");
-class Kredit extends CI_Controller{
-    function __construct(){
+class Kredit extends CI_Controller
+{
+    function __construct()
+    {
         parent::__construct();
         $this->load->model('M_kredit');
     }
 
-    function pengajuanRekening(){
-		$data['path'] = 'kredit/form_kredit_baru';
-		$this->load->view('master_template',$data);
-	}
+    function pengajuanRekening()
+    {
+        $data['path'] = 'kredit/form_kredit_baru';
+        $this->load->view('master_template', $data);
+    }
 
-    function daftarNominatif(){
+    function daftarNominatif()
+    {
         $data['title'] = 'Daftar Nominatif Kredit/Pembayaran';
         $data['path'] = "kredit/v_daftar_nominatif_kredit";
         // $data['simpanan_wajib'] = $this->M_simpanan_wajib->getSimpananpokok();
         $this->load->view('master_template', $data);
     }
 
-    function detail(){
+    function detail()
+    {
         $data['title'] = 'Detail Nominatif Kredit/Pembayaran';
         $data['path'] = "kredit/detail_daftar_nominatif_kredit";
         $this->load->view('master_template', $data);
     }
+
+    //Form rekening baru
+    function rekening_baru()
+    {
+        $data['path'] = 'kredit/form_kredit_baru';
+        $data['anggota'] = $this->M_anggota->getAnggota();
+        $this->load->view('master_template', $data);
+    }
+
+    //Ajax Get Data Anggota
+    function manageAjaxGetDataAnggota()
+    {
+        $no_anggota = $this->input->post('id');
+        $data['anggota'] = $this->M_anggota->get1Anggota(array('no_anggota' => $no_anggota));
+        $this->load->view('kredit/get_data_anggota', $data);
+    }
 }
-?>
---
+++ application/views/kredit/get_data_anggota.php
@@ -0,0 +1,10 @@
+<?php foreach ($anggota as $a) { ?>
+    <div class="col-md-6">
+        <label for="">Nama</label>
+        <input type="text" name="nama" class="form-control" value="<?php echo $a->nama; ?>" required readonly>
+    </div>
+    <div class="col-md-6">
+        <label for="">Alamat</label>
+        <textarea name="alamat" class="form-control" style="height:100px;max-height:100px;" required readonly><?php echo $a->alamat; ?></textarea>
+    </div>
+<?php } ?>
